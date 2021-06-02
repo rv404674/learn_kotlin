5ec637c81fbe703c644afaf4b87496e0d40ba1e8
Rahul Verma <rv404674@gmail.com>
2021-06-02 17:07:40 +0530

    Basics about variable  in kotlin

---
+++ src/main/kotlin/ControlFlow.kt
@@ -0,0 +1,13 @@
+
+fun main(){
+    val name:String = "Rahul"
+    val greeting: String?= null
+
+    if (greeting != null){
+        println(greeting)
+    }else{
+        println("Nothing")
+    }
+
+    println(name)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+var greeting: String = "GOod Morning"
+
+// NOTE: by default all types are non null. To put a null, you need to put a ?
+// var curse_word: String? = null
+var firstName: String? = "Rahul"
+
+fun main(){
+    println("hello kotlin")
+
+    /**
+     * val - read only/immutable
+     * var - mutable
+     */
+    val name2: String = "Rahul Verma"
+    println(greeting)
+    println(name2)
+    println(firstName)
+
+    // NOTE: kotlin can infer a variable type as well.
+    val shortcutName = "Shortcut Declaration"
+    println(shortcutName)
+}
+
