d9402f9be95f79d399c2503daf1d6fe14ea310c3
Rahul Verma <rv404674@gmail.com>
2021-06-05 12:48:15 +0530

    Learnt about map and list in kotlin

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -19,6 +19,32 @@ fun main(){
         println("$interestingThing is at $index")
     }
 
-     
+    // List
+    val places = listOf("Canberra", "Istnabul", "NewDelhi")
+    places.forEach { place ->
+        println(place)
+    }
+
+
+    // NOTE:MAP
+    val nameMap =  mapOf<Int, String>(1 to "Rahul Verma", 2 to "Sachin Verma", 3 to "Kamla Verma")
+    nameMap.forEach { (key, value) ->
+        println("$key to $value")
+    }
+
+
+    // NOTE: by default kotlin's collections are immutable. You can't add/subtract values.
+    // places.add -> no such method
+    val places2 = mutableListOf("Canberra", "germany")
+    places2.add("zurich")
+
+    val map = mutableMapOf(1 to 10, 2 to 100)
+    map.put(5, 100)
+
+
+
+
+
+
 
 }
