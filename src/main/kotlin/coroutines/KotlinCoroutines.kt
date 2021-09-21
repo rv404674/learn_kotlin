78baa6c8fd6adf8136c9281d5fe8e0bc4df10aab
Rahul Verma <rv404674@gmail.com>
2021-09-21 18:13:36 +0530

    learnt about suspend and runBlocking

--- src/main/kotlin/coroutines/KotlinCoroutines.kt
+++ src/main/kotlin/coroutines/KotlinCoroutines.kt
@@ -1,7 +1,9 @@
 package coroutines
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
 fun main() {
@@ -11,16 +13,21 @@ fun main() {
 
     // NOTE: by default The application wont wait for the couroutine to complete.
     // start coroutines on background thread. (worker thread)
-    GlobalScope.launch {
+    GlobalScope.launch { // Thread T1
         println("Fake work starts: ${Thread.currentThread().name}")
 
-        // NOTE: this will block all the other coroutines running inside the thread.
-        Thread.sleep(2000) // Pretend some fake work, file upload maybe
-        println("Fake work ended: ${Thread.currentThread().name}")
+        // Coroutine is suspended, but the thread T1 is free.
+        delay(2000) // Pretend some fake work, file upload maybe
+        println("Fake work ended: ${Thread.currentThread().name}") // Either T1 or some other thread. Happens after
+    // delay/suspend functoin
     }
 
-    // Blocks the current thread and waits for the coroutine to finish.
-    Thread.sleep(4000)
+    // Creates a coroutine that block current main thread.
+    // Same as thread.sleep()
+    // Not the right way to wait for other coroutines.
+    runBlocking {
+        delay(4000)
+    }
     println("Main Program ended: ${Thread.currentThread().name}")
 
 }
