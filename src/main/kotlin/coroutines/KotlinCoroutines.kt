7e733850b53e943d8e08b8c61de9a8eb81f62018
Rahul Verma <rv404674@gmail.com>
2021-09-22 16:57:02 +0530

    Added some changes according to launch{}

--- src/main/kotlin/coroutines/KotlinCoroutines.kt
+++ src/main/kotlin/coroutines/KotlinCoroutines.kt
@@ -1,9 +1,6 @@
 package coroutines
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlin.concurrent.thread
 
 fun main() {
@@ -12,7 +9,12 @@ fun main() {
     runBlocking { // Created a coroutine that blocks the main thread.
         println("Main Program starts: ${Thread.currentThread().name}") //main thread.
 
-        GlobalScope.launch { // Thread T1
+        // Fake work starts: DefaultDispatcher-worker-1
+        // NOTE: instead of this, you will get
+        // Fake work starts: main
+        // This is because launch{} launches a coroutine in the parent's scope. As the parent coroutine ->
+        // runBlocking{} is running on main thread, i.e child coroutine (launch{}), also runs on the main thread.
+        val job: Job = launch { // Thread T1
             println("Fake work starts: ${Thread.currentThread().name}")
 
             // Coroutine is suspended, but the thread T1 is free.
@@ -21,7 +23,9 @@ fun main() {
             // delay/suspend functoin
         }
 
-        delay(4000) //main thread.
+        //delay(4000) //main thread.
+        // job.cancel()
+        job.join() // will wait for coroutine job to finish.
 
         println("Main Program ended: ${Thread.currentThread().name}")
     }
